1b8406262d19cb9140ac2a7922621e9a44f56ed6
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-15 20:46:28 -0400

    implement ejs and create routes

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 var express = require("express");
 var app = express();
 
+app.set("view engine", "ejs");
+
 
 
 //#########################################################
@@ -14,10 +16,20 @@ var app = express();
 //
 //#########################################################
 app.get("/", function(req, res){
-  res.send("This will be the landing page.");
+  res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+  // temp campgrounds array
+  var campgrounds = [
+    {name: "Salmon Creek", image: "http://www.hike-nh.com/faq/safety/bear.jpg"},
+    {name: "Granite Hill", image: "http://hikethewhites.com/tarryhoweekend/fw04.jpg"},
+    {name: "Mount Moody", image: "http://www.dec.ny.gov/images/permits_ej_operations_images/kennethwilsonpav.jpg"},
+    {name: "Schweady Lake", image: "http://img1.sunset.timeinc.net/sites/default/files/styles/1000x1000/public/image/2016/10/main/hoodview-campground-0510.jpg?itok=B8Eb65Uf"}
+  ];
 
+  res.render("campgrounds");
+});
 
 //#########################################################
 //
