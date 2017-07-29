2feadde15ad7d4510deac055bfdd7d1a2deb970a
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-28 20:11:50 -0400

    execute #seedDB in app.js

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ var express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
-    Campground = require("./models/campground")
+    Campground = require("./models/campground"),
+    seedDB     = require("./seeds")
 
+seedDB();
 // Connect to yelp_camp db (initial run will create db)
 mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true})
 
