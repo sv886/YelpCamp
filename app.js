47cae12849375498d6bddd19dd82e6f689e2d7b6
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-29 13:36:17 -0400

    update nested comments new route to serve campground data

--- app.js
+++ app.js
@@ -87,7 +87,14 @@ app.get("/campgrounds/:id", function(req,res){
 
 // Comments Routes ========================================
 app.get("/campgrounds/:id/comments/new", function(req, res){
-  res.render("comments/new");
+  // Find campground by id
+  Campground.findById(req.params.id, function(err, campground){
+    if(err) {
+      console.log(err)
+    } else {
+      res.render("comments/new", {campground: campground});
+    }
+  });
 });
 
 //#########################################################
