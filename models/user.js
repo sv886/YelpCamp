1ecd0164cba8a392ca1d2935ae1159466487e983
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-30 14:07:33 -0400

    update user model with passport functionality

--- models/user.js
+++ models/user.js
@@ -1,8 +1,13 @@
 var mongoose = require("mongoose");
+// Add in passport-local plugin
+var passportLocalMongoose = require("passport-local-mongoose");
 // Create schema for user
 var userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
+
+// Make passport methods available to our User model
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("User", userSchema);
