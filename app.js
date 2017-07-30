90af002364b592ef0f3784f71f68388a2748055c
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-07-30 14:40:47 -0400

    create user registration routes

--- app.js
+++ app.js
@@ -143,6 +143,26 @@ app.post("/campgrounds/:id/comments", function(req, res){
   });
 });
 
+// Auth Routes ========================================
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  // Passport's #register will save new user obj and hashed password
+  User.register(newUser, req.body.password, function(err, user){
+    if(err) {
+      console.log(err);
+      return res.render("register");
+    }
+    // Login user, authenticate and redirect
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    });
+  });
+});
+
 
 
 //#########################################################
