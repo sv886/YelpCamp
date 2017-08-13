a642390a5fe3b06ba96106967dcdb82902c74218
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-08-13 13:26:13 -0400

    extract middleware logic into new mw index

---
+++ middleware/index.js
@@ -0,0 +1,63 @@
+//#########################################################
+//
+// Middleware
+//
+//#########################################################
+var middlewareObj = {};
+
+middlewareObj.checkCampgroundOwnership = function(req, res, next){
+  // is user logged in?
+  if(req.isAuthenticated()){
+    Campground.findById(req.params.id, function(err, foundCampground){
+      if(err) {
+        // sends user back to last successful request
+        res.redirect("back");
+      } else {
+        // Note that if(foundCampground.author.id === req.user._id) is
+        // actually mongooseObject === String evaluation and will not work.
+        // Use mongoose's #equals method to accomplish auth check.
+        if(foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
+
+middlewareObj.checkCommentOwnership = function(req, res, next){
+  // is user logged in?
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err) {
+        // sends user back to last successful request
+        res.redirect("back");
+      } else {
+        // Note that if(foundComment.author.id === req.user._id) is
+        // actually mongooseObject === String evaluation and will not work.
+        // Use mongoose's #equals method to accomplish auth check.
+        if(foundComment.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
+
+middlewareObj.isLoggedIn = function(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
+
+
+module.exports = middlewareObj;
