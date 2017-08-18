3689a019077702dbd436046dbfd8049c8785b1c2
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-08-18 16:31:48 -0400

    install/config connect-flash pkg for flash msgs in views

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express        = require("express"),
     bodyParser     = require("body-parser"),
     mongoose       = require("mongoose"),
     passport       = require("passport"),
+    flash          = require("connect-flash"),
     LocalStrategy  = require("passport-local"),
     methodOverride = require("method-override")
 
@@ -27,8 +28,9 @@ mongoose.connect("mongodb://localhost/yelp_camp", {useMongoClient: true})
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + "/public"))
+app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+app.use(flash());
 // seedDB();
 
 
